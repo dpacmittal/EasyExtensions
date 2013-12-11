5a0078adc58fa6a5074906e655b63652012f34bd
Deepak Mittal <dpac.mittal2@gmail.com>
2013-12-11 17:02:26 +0530

    Options page now displays list of extensions which are searchable

---
+++ js/extensions.js
@@ -0,0 +1,12 @@
+function ExtensionsCtrl($scope) {
+        $scope.extensions = [];
+        chrome.management.getAll(
+                function(info){
+                        $scope.$apply(function(){
+                                $scope.extensions = info;
+                        });
+                        //console.log(info);
+                }
+        );
+
+}
